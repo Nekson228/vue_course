a3141daa95a2d997c89d039621a786446ab8d007
neepaw <bangerwockeez@gmail.com>
2023-03-08 16:26:35 +0300

    first one!

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import Counter from "@/components/Counter.vue";
+</script>
+
+<template>
+  <h1>Hello!</h1>
+  <Counter></Counter>
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+    text-align: center;
+  }
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref, watchEffect} from "vue";
+
+const counter = ref(0)
+const date = ref(new Date().toLocaleTimeString())
+
+watchEffect(() => {
+  setInterval(() => {
+    date.value = new Date().toLocaleTimeString()
+  }, 1000)
+})
+</script>
+
+<template>
+  <button @click="counter++">+</button>
+  <button @click="counter--">-</button>
+  <h1>Счетчик: {{ counter }}</h1>
+  <h1>Время: {{ date }}</h1>
+</template>
+
+<style scoped>
+  h1 {
+    font-family: monospace;
+  }
+  button {
+    background-color: black;
+    color: white;
+    border-radius: 10px;
+    border: 2px solid grey;
+    height: 30px;
+    width: 30px;
+  }
+</style>
