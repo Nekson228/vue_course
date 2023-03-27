0d4aa513182b43f2dbdff7681387feb5f87d67f1
Nekson228 <bangerwockeez@gmail.com>
2023-03-27 15:59:13 +0300

    hw2: done (for sure)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script setup>
-  import Counter from "@/components/Counter.vue";
+import css from "./App.module.css"
+import {ref} from "vue";
+
+const text = ref("")
+const cards = [
+  {id: 0, data: "Lorem"},
+  {id: 1, data: "Ipsum"},
+  {id: 2, data: "Dolor"},
+  {id: 3, data: "Sit"},
+  {id: 4, data: "Amet"},
+  {id: 5, data: "Lorem21"},
+  {id: 6, data: "!Ipsum_!"},
+  {id: 7, data: "__Dolor_"},
+  {id: 8, data: "%Sit%"},
+  {id: 9, data: "##Amet#"},
+]
 </script>
 
 <template>
-  <h1>Hello!</h1>
-  <Counter></Counter>
-</template>
+  <div :class="css.data">
+    <span style="margin-right: 10px">Search:</span>
+    <input type="text" v-model="text">
+  </div>
 
-<style scoped>
-  h1 {
-    color: red;
-    text-align: center;
-  }
-</style>
+  <template v-for="card in cards" :key="card.id">
+    <div :class="[css.card]" v-if="card.data.includes(text)">
+      {{ card.id }}: {{ card.data }}
+    </div>
+  </template>
+</template>
